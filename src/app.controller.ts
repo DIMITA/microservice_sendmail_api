96815b19efa33911061b5ac0d8dd1e6de82200b2
DIMITA <dimitriomorales01@gmail.com>
2023-11-24 21:50:44 +0100

    Écoute du message pour appel du service d'envoi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import { MailService } from './mail/mail.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private mailservice: MailService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('notifications')
+  getNotifications(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log(`Pattern: ${context.getPattern()}`);
+    console.log(`Message: ${context.getMessage()}`);
+    console.log(`data: ${data}`);
+    this.mailservice.sendUserConfirmation(JSON.parse(data));
   }
 }
